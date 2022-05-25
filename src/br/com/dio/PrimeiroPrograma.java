f734604aa626c415886c2b3aa927018e10756d64
Danilo jesus <danilocuencas@hotmail.com>
2022-05-25 13:19:56 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,9 +6,45 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
-
-        int a = 5;
-        int b = 3;
-        System.out.println("hello World!" + (a + b));
+       // int i = 98;
+     //   int a = 5;
+       // int b = 3;
+        //System.out.println("hello World!" + (a + b));
+        Livro livro1 = new Livro("um nome qualquer", 987);
+        System.out.println(livro1);
     }
 }
+class  Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
